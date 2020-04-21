59f1b50df4a770fce3818a460e7cea5a5b50b42c
Jing Huang <jh3ex@virginia.edu>
2020-04-20 22:18:50 -0400

    april20

--- cylinder/scripts/motion_model.py
+++ cylinder/scripts/motion_model.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
+"""
+Human motion control ros node
+
+Jing Huang
+"""
 
 import rospy
 # from gazebo_msgs.srv import GetModelState, GetModelStateResponse
@@ -7,24 +12,15 @@ from gazebo_msgs.msg import ModelState
 import numpy as np
 
 
-# def callback(data):
-#     rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
-#
-# def listener():
-#     rospy.init_node('listener', anonymous=True)
-#     rospy.Subscriber('/human/get_model_state', ModelState, callback)
-#
-#     rospy.spin()
-
 class HumanMotion_Gazebo:
-    def __init__(self, model_name, motion_model, pmm):
-        
-        self.motion_model = pmm
-        
+    def __init__(self, model_name, motion_model):
+
+        self.motion_model = motion_model
+
         self.model_name = model_name
-        
+
         self.x = ModelState()
-        
+
         self.x.model_name = self.model_name
         # self.x.pose.position.x = 0
         # self.x.pose.position.y = 0
@@ -33,103 +29,110 @@ class HumanMotion_Gazebo:
         # self.x.pose.orientation.y = 0
         # self.x.pose.orientation.z = 0
         # self.x.pose.orientation.w = 0
-        
+
+        self.info = ['STOP', 'WALK']
+
     def walk(self, update_rate):
         # t_1 = self.get_clock()
-        
+
         # The rate (Hz) the model updates human states
+        rospy.init_node('human_control', anonymous=True)
         rate = rospy.Rate(update_rate)
         delta_t = 1/update_rate
-        
-        x, y, theta= self.get_human_state()
-        
+
+        position, orientation = self.get_human_state()
+
+        self.x.pose.orientation = orientation
+
+        x = position.x
+        y = position.y
+        theta = 0
         # self.x.pose.position.x = x + 0.1
         # self.x.pose.position.y = y + 0.1
         # self.set_human_state(self.x)
-        
+
         while not rospy.is_shutdown():
             # Update human states
-            x, y, theta = self.motion_model(x, y, theta, delta_t)
+            x, y, theta = self.motion_model.take_one_step(x, y, theta, delta_t)
             # Update pose msg
             self.x.pose.position.x = x
             self.x.pose.position.y = y
-            self.x.pose.orientation.z = theta
+            # self.x.pose.orientation.z = theta
             # Ask gazebo to update human state
             self.set_human_state(self.x)
+
+            print("[%s] Human position: x=%g, y=%g, theta=%g"\
+             %(self.info[self.motion_model.walking], x, y, theta))
+            # print(self.x)
+
             rate.sleep()
-            
-        
-        
-    
+
+
+
+
     def get_human_state(self):
         rospy.wait_for_service('gazebo/get_model_state')
-        
+
         try:
             get_model_state = rospy.ServiceProxy('gazebo/get_model_state', GetModelState)
             resp1 = get_model_state(self.model_name, 'world')
-            
-            return resp1.pose.position.x, resp1.pose.position.x, resp1.pose.orientation.z
+            # print(resp1)
+            return resp1.pose.position, resp1.pose.orientation
         except rospy.ServiceException:
             print("Service call failed")
-        
-        
+
+
     def set_human_state(self, x):
         rospy.wait_for_service('gazebo/set_model_state')
-        
+
         try:
             set_model_state = rospy.ServiceProxy('gazebo/set_model_state', SetModelState)
             set_model_state(x)
     #        return resp1.pose
         except rospy.ServiceException:
             print("Service call failed")
-            
+
     def get_clock(self):
         pass
 
-# def get_clock():
-#     # Get the simulation clock time
-#     pass
-
-
 class ProbMotionModel:
-    def __init__(self, v_mu, v_sigma, theta_dot_mu, theta_dot_sigma, tans_prob):
+    def __init__(self, v_mu, v_sigma, theta_dot_mu, theta_dot_sigma, trans_prob):
         self.v_mu = v_mu
         self.v_sigma = v_sigma
         self.theta_dot_mu = theta_dot_mu
         self.theta_dot_sigma = theta_dot_sigma
         self.trans_prob = trans_prob
-        
+
         self.walking = 1
-    
-    
+
+
     def take_one_step(self, x, y, theta, delta_t):
         # Choose if human stops
-        self.walking = np.random.choice(1, p=self.trans_prob[self.walking, :])
+        self.walking = np.random.choice([0, 1], p=self.trans_prob[self.walking, :])
         if self.walking == 1:
-            
+
             theta_dot = np.random.normal(loc=self.theta_dot_mu, scale=self.theta_dot_sigma)
             theta += theta_dot * delta_t
 
             v = np.random.normal(loc=self.v_mu, scale=self.v_sigma)
-            x += v * np.sin(theta) * delta_t
-            y += v * np.cos(theta) * delta_t
-            
+            x += v * np.cos(theta) * delta_t
+            y += v * np.sin(theta) * delta_t
+
         return x, y, theta
-            
-            
+
+
 
 if __name__ == '__main__':
-    
+
     # Motion model parameter
-    v_mu = 1.47
-    v_sigma = 0.12
+    v_mu = 0.1
+    v_sigma = 0.012
     theta_dot_mu = 0
-    theta_dot_sigma = 0.1
+    theta_dot_sigma = 0.02
     trans_prob = np.array([[0.5, 0.5], [0.1, 0.9]])
-    
+
     pmm = ProbMotionModel(v_mu, v_sigma, theta_dot_mu, theta_dot_sigma, trans_prob)
-    
+
     hm_gazebo = HumanMotion_Gazebo('cylinder', pmm)
     hm_gazebo.walk(0.5)
     # listener()
-    
---
+++ cylinder/scripts/test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 12 16:55:21 2020
+
+@author: jing
+"""
+
+from gazebo_msgs.msg import ModelState
+
+a = ModelState()
+print(a)
